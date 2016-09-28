05515236dc8178ea873ee0a470b5b9b53be53dc5
Sanderfer Chau <sanderfer.chau@gmail.com>
2016-09-28 15:45:08 -0700

    Clean up code

--- db/migrate/20160423094502_create_measurements.rb
+++ db/migrate/20160423094502_create_measurements.rb
@@ -2,6 +2,15 @@ class CreateMeasurements < ActiveRecord::Migration
   def change
     create_table :measurements do |t|
       t.integer :weight
+      t.integer :height
+      t.integer :neck
+      t.integer :waist
+      t.integer :left_arm
+      t.integer :right_arm
+      t.integer :hips
+      t.integer :chest
+      t.integer :right_thigh
+      t.integer :left_thigh
       t.integer :body_fat
       t.integer :dead_lift
       t.integer :bench_press
