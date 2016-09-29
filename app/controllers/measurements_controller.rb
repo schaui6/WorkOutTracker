753f4d02d1a23621d33ee04a5daa3796daf81178
Sanderfer Chau <sanderfer.chau@gmail.com>
2016-09-28 20:29:00 -0700

    Add calculators for BMI, Target HR

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -3,6 +3,10 @@ class MeasurementsController < ApplicationController
   def index
     @user = User.find_by(id: params[:user_id])
     @measurements = @user.measurements
+    @last_measurement = @measurements.last
+    @low_int_target_zone = find_low_int_target_hr(find_max_hr(@last_measurement))
+    @high_int_target_zone = find_high_int_target_hr(find_max_hr(@last_measurement))
+    @bmi = find_BMI(@last_measurement)
   end
 
   def new
@@ -44,9 +48,33 @@ class MeasurementsController < ApplicationController
       end
     end
 
-    private
+  private
 
   def measurement_params
-    params.require(:measurement).permit(:weight, :waist, :neck, :chest, :hips, :body_fat, :photo, :age)
+    params.require(:measurement).permit(:weight, :waist, :neck, :chest, :hips, :body_fat, :photo, :age, :height)
+  end
+
+  def find_max_hr(measurement)
+    220 - measurement.age
+  end
+
+  def find_low_int_target_hr(max_hr)
+    max_hr * 0.70
+  end
+
+  def find_high_int_target_hr(max_hr)
+    max_hr * 0.85
+  end 
+
+  def in_to_m(inches)
+    inches / 39.370
+  end
+
+  def lbs_to_kg(lbs)
+    lbs / 2.2046226218
+  end
+
+  def find_BMI(measurement)
+    ( ( lbs_to_kg(measurement.weight) / in_to_m(measurement.height) ) / in_to_m(measurement.height) )
   end
 end
