1272a82bc83386a773f0563ff2c106ac11a1fa9f
Ian Thorp <IanMThorp@gmail.com>
2016-11-16 20:26:42 -0800

    Change phase workouts to card-style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -62,6 +62,7 @@ var markComplete = function(event){
     console.log(data);
     if (data) {
       this.prop('checked', true);
+      this.parents('.workout-info').hide();
     } else {
       this.removeAttr('checked');
     }
